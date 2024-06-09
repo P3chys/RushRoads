84238d3265d4606cc92b4c189c84bc6d91a3da6a
Adam Pech <xpech@mendelu.cz>
2024-06-09 22:25:06 +0200

    moved movement to separate file and added music player

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import * as THREE from 'three';
 import * as TWEEN from '@tweenjs/tween.js'
 import { Lights } from './helpers/init_lights';
 import { Load_model } from './helpers/import_player';
-import { Move} from './move.js';
-
+import { playMusic } from './music';
+import {AnimateMovement} from './move';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -16,42 +16,28 @@ document.body.appendChild( renderer.domElement );
 camera.position.z = 50;
 camera.position.y = 20;
 
-Lights(scene);
-
 var player = new THREE.Object3D;
 player.name='player';
 scene.add(player);
 
+Lights(scene);
+
 Load_model(player,0,0,0);
 
+playMusic(camera);
 
-console.log(scene.getObjectByName('player').position);
-
-
-
-function onClick(amount) {
-
-  new TWEEN.Tween(player.position)
-    .to(player.position.clone().setX(player.position.x+amount), 500)
-    .onStart(function() {
-    })
-    .onComplete(function() {
-    })
-    .start();
+document.onkeydown = function (e) {
+    if (e.key === 'd') {
+        AnimateMovement(player,5)
+    }
+    if (e.key === 'a') {
+        AnimateMovement(player,-5)
+    }
 }
- 
+
+
 function animate() {
     requestAnimationFrame( animate );
-	renderer.render( scene, camera );
-    
-
-    document.onkeydown = function(e){
-        if(e.key === 'd'){
-            onClick(5)
-        }
-        if(e.key === 'a'){
-            onClick(-5)
-        }
-    }
     TWEEN.update()
+	renderer.render( scene, camera );
 }
---
+++ src/music.js
@@ -0,0 +1,18 @@
+import * as THREE from 'three';
+
+export function playMusic(camera){
+    const listener = new THREE.AudioListener();
+    camera.add( listener );
+    
+    // create a global audio source
+    const sound = new THREE.Audio( listener );
+    
+    // load a sound and set it as the Audio object's buffer
+    const audioLoader = new THREE.AudioLoader();
+    audioLoader.load( './001.mp3', function( buffer ) {
+        sound.setBuffer( buffer );
+        sound.setLoop( true );
+        sound.setVolume( 0.3 );
+        sound.play();
+    });
+}
