1a5aff805c12bfee26fa34163e31cc0faca9097b
Adam Pech <xpech@mendelu.cz>
2024-06-17 00:12:43 +0200

    camera tweaks

--- src/models/camera.js
+++ src/models/camera.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 
-const cameraFOV = 75, cameraNear = 0.1, cameraFar = 2000;
-const cameraZ=50, cameraY=20;
+const cameraFOV = 85, cameraNear = 0.1, cameraFar = 2000;
+const cameraZ=50, cameraY=30;
 
 export function initCamera(scene){
     let camera = new THREE.PerspectiveCamera(cameraFOV, window.innerWidth / window.innerHeight, cameraNear, cameraFar);
